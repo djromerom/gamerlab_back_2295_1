7356c6af821f3a7b8703ad1bf2c70acd11585f66
Orlando Jr. Rengifo <107599003+OrlandoJrRengifo@users.noreply.github.com>
2025-05-04 16:54:11 -0500

    envio de correros

--- src/Grupo 10/equipo/update-equipo.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEquipoDto } from './dto/create-equipo.dto';
-
-export class UpdateEquipoDto extends PartialType(CreateEquipoDto) {}
